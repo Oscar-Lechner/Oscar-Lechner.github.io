21753f6b8efef4a41989ffeefbf1a02bd691588b
Oscar Lechner <osle1464@colorado.edu>
2024-04-04 12:48:52 -0600

    image carousel

--- wa/wa11/wa11.js
+++ wa/wa11/wa11.js
@@ -1,17 +1,51 @@
 let images = ['wa11-img/image1.JPG', 'wa11-img/image2.JPG', 'wa11-img/image3.JPG', 'wa11-img/image4.JPG', 'wa11-img/image5.JPG', 'wa11-img/image6.JPG', 'wa11-img/image7.JPG'];
 let currentIndex = 0;
 
-function changeImage(src) {
-    document.getElementById("mainImage").src = src;
-    currentIndex = images.indexOf(src);
+function changeImage(index) {
+    document.getElementById("mainImage").src = images[index];
+    currentIndex = index;
+    updateThumbnails();
 }
 
 function nextImage() {
     currentIndex = (currentIndex + 1) % images.length;
-    changeImage(images[currentIndex]);
+    changeImage(currentIndex);
 }
 
 function previousImage() {
     currentIndex = (currentIndex - 1 + images.length) % images.length;
-    changeImage(images[currentIndex]);
+    changeImage(currentIndex);
 }
+
+function updateThumbnails() {
+    let thumbnailContainer = document.querySelector('.thumbnail-container');
+    let thumbnails = thumbnailContainer.querySelectorAll('.thumbnail');
+
+    // Reset active class for all thumbnails
+    thumbnails.forEach(thumbnail => thumbnail.classList.remove('active'));
+
+    // Set active class for the current thumbnail
+    let currentThumbnail = thumbnails[currentIndex];
+    if (currentThumbnail) {
+        currentThumbnail.classList.add('active');
+    }
+
+    // Update thumbnail positions
+    for (let i = 0; i < thumbnails.length; i++) {
+        let thumbnailIndex = (currentIndex + i) % images.length;
+        thumbnails[i].src = images[thumbnailIndex];
+    }
+}
+
+function toggleMode() {
+    let body = document.body;
+    body.classList.toggle('dark-mode');
+
+    let thumbnails = document.querySelectorAll('.thumbnail');
+    thumbnails.forEach(thumbnail => {
+        thumbnail.classList.toggle('dark-thumbnail');
+    });
+}
+
+// Initialize the carousel
+changeImage(currentIndex);
